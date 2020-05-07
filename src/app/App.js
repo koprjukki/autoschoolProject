e6bf7668082cc9e35fecf7536993868b033c63c4
Ivan Chernovkiy <vano322776@gmail.com>
2020-05-07 19:41:47 +0300

    change project but slider not work

--- src/app/App.js
+++ src/app/App.js
@@ -7,34 +7,33 @@ import city from "../json/city.json";
 import "./_app.sass";
 
 class App extends Component {
-	state = {
-		data: [],
-	};
+  state = {
+    data: [],
+  };
 
-	// Form //
-	handelGetUserInfo = (data) => {
-		this.setState((prevState) => ({
-			data: [...prevState.data, data],
-		}));
-	};
+  // Form //
+  handelGetUserInfo = (data) => {
+    this.setState((prevState) => ({
+      data: [...prevState.data, data],
+    }));
+  };
 
-	render() {
-		const { data } = this.state;
-		console.log(data);
-		return (
-			<>
-				<Slider />
-				<Form getUserInfo={this.handelGetUserInfo} />
-				<Nav />
-				<Switch>
-					{/* <Route path="/" exact component="HomePage" /> */}
-					<Route path="/services" component="ServicesPage" />
-					<Route path="/about" component="AboutPage" />
-					<Route path="/contacts" component="ContactsPage" />
-				</Switch>
-			</>
-		);
-	}
+  render() {
+    const { data } = this.state;
+    console.log(data);
+    return (
+      <>
+        <Form getUserInfo={this.handelGetUserInfo} />
+        <Nav />
+        <Switch>
+          {/* <Route path="/" exact component="HomePage" /> */}
+          <Route path="/services" component="ServicesPage" />
+          <Route path="/about" component="AboutPage" />
+          <Route path="/contacts" component="ContactsPage" />
+        </Switch>
+      </>
+    );
+  }
 }
 
 export default App;
